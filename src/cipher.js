433af217544d4686a9b0eacce7f304edfef9a8ea
MercedesDavila <mmdavila@outlook.com>
2019-06-14 09:49:16 -0500

    Actualizacion src

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,43 @@
 window.cipher = {
-  // ... 
+
+
+    encode: (offset, string) => {
+        let textCoded = '';
+        let newString = string.toUpperCase();
+
+        for (let i = 0; i < string.length; i++) {
+
+            let alphabetIndex = newString.charCodeAt(i);
+            let cipherFormula = (alphabetIndex - 65 + offset) % 26 + 65;
+            let OffsetToText = String.fromCharCode(cipherFormula);
+
+            if (newString === ' ') {
+                textCoded += ' ';
+            } else {
+                textCoded += OffsetToText;
+            }
+        }
+
+        return textCoded;
+    },
+
+    decode: (offset, string) => {
+        let textDecoded = '';
+        let newString = string.toUpperCase();
+
+        for (let i = 0; i < string.length; i++) {
+
+            let alphabetIndex = newString.charCodeAt(i);
+            let decipherFormula = (alphabetIndex - 65 - offset + 52) % 26 + 65;
+            let decodedtoText = String.fromCharCode(decipherFormula);
+
+            if (newString === ' ') {
+                textDecoded += ' ';
+            } else {
+                textDecoded += decodedtoText;
+            }
+        }
+
+        return textDecoded;
+    },
 };
---
+++ src/index.js
@@ -0,0 +1,26 @@
+const encodebtn = document.getElementById('btn_encode');
+let codeOffSet = document.getElementById('n_offset_e');
+let textToCode = document.getElementById('box-msg-encode');
+const decodebtn = document.getElementById('btn_decode');
+let decodeOffSet = document.getElementById('n_offset_d');
+let textToDecode = document.getElementById('box-msg-decode');
+
+const printEncodeMessage = () => {
+    const blankMsg = document.getElementById('msg-encode-check');
+    let codeOffSetNumber = parseInt(codeOffSet.value);
+    let textCoded = cipher.encode(codeOffSetNumber, textToCode.value);
+    blankMsg.innerHTML = textCoded;
+}
+
+encodebtn.addEventListener("click", printEncodeMessage);
+
+
+
+const printDecodeMessage = () => {
+    const blankMsg = document.getElementById('msg-decode-check');
+    let decodeOffSetNumber = parseInt(decodeOffSet.value);
+    let textDecoded = cipher.decode(decodeOffSetNumber, textToDecode.value);
+    blankMsg.innerHTML = textDecoded;
+}
+
+decodebtn.addEventListener("click", printDecodeMessage);
